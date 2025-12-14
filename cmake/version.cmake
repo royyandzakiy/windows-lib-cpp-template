64c526a15fe52dc2f9c2124105d6b720cb79d850
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2025-12-14 10:30:08 +0700

    fix(doxyfile): enable generate doxygen docs with proper library version. adding PARENT_SCOPE to script

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -9,34 +9,41 @@ function(generate_version_header)
         list(LENGTH VERSION_LIST VERSION_LENGTH)
         
         if(VERSION_LENGTH GREATER 0)
-            list(GET VERSION_LIST 0 PROJECT_VERSION_MAJOR)
-        else()
-            set(PROJECT_VERSION_MAJOR 0)
-        endif()
-        
-        if(VERSION_LENGTH GREATER 1)
-            list(GET VERSION_LIST 1 PROJECT_VERSION_MINOR)
-        else()
-            set(PROJECT_VERSION_MINOR 0)
-        endif()
-        
-        if(VERSION_LENGTH GREATER 2)
-            list(GET VERSION_LIST 2 PROJECT_VERSION_PATCH)
-        else()
-            set(PROJECT_VERSION_PATCH 0)
-        endif()
+			list(GET VERSION_LIST 0 PROJECT_VERSION_MAJOR)
+			set(PROJECT_VERSION_MAJOR "${PROJECT_VERSION_MAJOR}" PARENT_SCOPE)
+		else()
+			set(PROJECT_VERSION_MAJOR 0 PARENT_SCOPE)
+		endif()
+			
+		if(VERSION_LENGTH GREATER 1)
+			list(GET VERSION_LIST 1 PROJECT_VERSION_MINOR)
+			set(PROJECT_VERSION_MINOR "${PROJECT_VERSION_MINOR}" PARENT_SCOPE)
+		else()
+			set(PROJECT_VERSION_MINOR 0 PARENT_SCOPE)
+		endif()
+			
+		if(VERSION_LENGTH GREATER 2)
+			list(GET VERSION_LIST 2 PROJECT_VERSION_PATCH)
+			set(PROJECT_VERSION_PATCH "${PROJECT_VERSION_PATCH}" PARENT_SCOPE)
+		else()
+			set(PROJECT_VERSION_PATCH 0 PARENT_SCOPE)
+		endif()
     else()
         # Fallback if version.txt doesn't exist
-        set(PROJECT_VERSION_MAJOR 0)
-        set(PROJECT_VERSION_MINOR 1)
-        set(PROJECT_VERSION_PATCH 0)
+        set(PROJECT_VERSION_MAJOR 0 PARENT_SCOPE)
+        set(PROJECT_VERSION_MINOR 1 PARENT_SCOPE)
+        set(PROJECT_VERSION_PATCH 0 PARENT_SCOPE)
         message(WARNING "version.txt not found, using default version: ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
     endif()
 
     # Set CMake project version
-    set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
-    
-    # Configure the version header in the source include directory
+    set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}" PARENT_SCOPE)
+
+	set(CMAKE_PROJECT_VERSION_MAJOR ${PROJECT_VERSION_MAJOR} PARENT_SCOPE)
+	set(CMAKE_PROJECT_VERSION_MINOR ${PROJECT_VERSION_MINOR} PARENT_SCOPE)
+	set(CMAKE_PROJECT_VERSION_PATCH ${PROJECT_VERSION_PATCH} PARENT_SCOPE)
+
+	# Configure the version header in the source include directory
     configure_file(
         ${CMAKE_CURRENT_SOURCE_DIR}/cmake/version.h.in
         ${CMAKE_CURRENT_SOURCE_DIR}/include/version.h
